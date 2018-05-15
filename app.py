edcc8a2c340bec8f0d09456c2107b6da7ec53e52
Yurii Denysenko <yuri.denysenko@s-o-services.com>
2018-05-15 13:11:50 +0300

    Queue Api complete

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 
-from models import db
-
 import resources
+from models import db
 
 app = Flask(__name__)
 app.config.from_object('default-config')
 app.config.from_envvar('QUEUE_CONFIG', silent=True)
 api = Api(app)
 
-api.add_resource(resources.Queue, '/queue')
+api.add_resource(resources.QueueCollection, '/queue/')
+api.add_resource(resources.QueueItem, '/queue/<int:queue_id>')
 
 db.init_app(app)
 
--- resources.py
+++ resources.py
@@ -1,11 +1,15 @@
-from flask_restful import Resource, reqparse, fields, marshal_with
+from flask_restful import Resource, reqparse, fields, marshal_with, abort
 
 import models
 
-parser = reqparse.RequestParser()
+get_parser = reqparse.RequestParser()
 
-parser.add_argument('start', type=int, default=0)
-parser.add_argument('limit', type=int, default=100)
+get_parser.add_argument('start', type=int, default=0)
+get_parser.add_argument('limit', type=int, default=100)
+
+post_queue_parser = reqparse.RequestParser()
+
+post_queue_parser.add_argument('name', type=str, required=True)
 
 queue_fields = {
     'id': fields.Integer,
@@ -14,7 +18,11 @@ queue_fields = {
     'updatedAt': fields.DateTime(dt_format='iso8601'),
 }
 
-get_fields = {
+queue_post_fields = {
+    'name': fields.String
+}
+
+queue_get_fields = {
     'start': fields.Integer,
     'limit': fields.Integer,
     'count': fields.Integer,
@@ -22,10 +30,10 @@ get_fields = {
 }
 
 
-class Queue(Resource):
-    @marshal_with(get_fields)
+class QueueCollection(Resource):
+    @marshal_with(queue_get_fields)
     def get(self):
-        args = parser.parse_args()
+        args = get_parser.parse_args()
         queues = models.Queue.query.offset(args['start']).limit(args['limit']).all()
 
         res = {
@@ -36,3 +44,43 @@ class Queue(Resource):
         }
 
         return res, 200 if len(queues) > 0 else 204
+
+    @marshal_with(queue_fields)
+    def post(self):
+        args = post_queue_parser.parse_args()
+        queue = models.Queue(name=args['name'])
+        models.db.session.add(queue)
+        models.db.session.commit()
+
+        return queue, 201
+
+
+class QueueItem(Resource):
+
+    def get_queue(self, queue_id):
+        queue = models.Queue.query.filter(models.Queue.id == queue_id).first()
+
+        if not queue:
+            abort(404)
+
+        return queue
+
+    @marshal_with(queue_fields)
+    def get(self, queue_id):
+        return self.get_queue(queue_id)
+
+    @marshal_with(queue_fields)
+    def put(self, queue_id):
+        args = post_queue_parser.parse_args()
+        queue = self.get_queue(queue_id)
+        queue.name = args['name']
+
+        models.db.session.commit()
+
+        return queue
+
+    def delete(self, queue_id):
+        queue = self.get_queue(queue_id)
+
+        models.db.session.delete(queue)
+        models.db.session.commit()
